42866df120baaaea7cd3ecd509e9f0bbd1acd3c8
georgefrg <georgeftw00@gmail.com>
2024-11-06 13:33:17 +0200

    implement metric and question card

--- lib/utils.ts
+++ lib/utils.ts
@@ -21,3 +21,29 @@ export const geteDeviconClassName = (techName: string) => {
     ? `${techMap[normalizedTechName]} colored`
     : "devicon-devicon-plain";
 };
+
+export const getTimeStamp = (date: Date): string => {
+  const now = new Date();
+  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  const years = Math.floor(diffInSeconds / (60 * 60 * 24 * 365));
+  const months = Math.floor(diffInSeconds / (60 * 60 * 24 * 30));
+  const days = Math.floor(diffInSeconds / (60 * 60 * 24));
+  const hours = Math.floor(diffInSeconds / (60 * 60));
+  const minutes = Math.floor(diffInSeconds / 60);
+  const seconds = diffInSeconds;
+
+  if (years >= 1) {
+    return years === 1 ? "1 year ago" : `${years} years ago`;
+  } else if (months >= 1) {
+    return months === 1 ? "1 month ago" : `${months} months ago`;
+  } else if (days >= 1) {
+    return days === 1 ? "1 day ago" : `${days} days ago`;
+  } else if (hours >= 1) {
+    return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
+  } else if (minutes >= 1) {
+    return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
+  } else {
+    return seconds === 1 ? "1 second ago" : `${seconds} seconds ago`;
+  }
+};
