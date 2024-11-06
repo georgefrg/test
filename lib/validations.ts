bf118bc14841ba1d19104feaf0c84ad563769d36
georgefrg <georgeftw00@gmail.com>
2024-11-06 14:01:10 +0200

    implement the ui of the question form

--- lib/validations.ts
+++ lib/validations.ts
@@ -73,3 +73,21 @@ export const SignUpSchema = z.object({
       message: "Password must contain at least one special character.",
     }),
 });
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Title is required" })
+    .max(100, { message: "Title cannot exceed 100 characters" }),
+
+  content: z.string().min(1, { message: "Body is required" }),
+  tag: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required" })
+        .max(30, { message: "Tag cannot contain more than 30 characters" })
+    )
+    .min(1, { message: "At least one tag must be specified" })
+    .max(3, { message: "Cannot add more than 3 tags" }),
+});
